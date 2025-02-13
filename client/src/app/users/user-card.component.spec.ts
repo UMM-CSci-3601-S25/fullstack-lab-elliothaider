2e5f4c8bf7d487ff19cedc4893c743dd4a8d94e1
Haider Merchant <merch103@morris.umn.edu>
2025-02-13 16:56:08 -0600

    Fix the errors in the starter code
    Co-authored-by: Elliot Hanson <hans7680@morris.umn.edu>

--- client/src/app/users/user-card.component.spec.ts
+++ client/src/app/users/user-card.component.spec.ts
@@ -1,13 +1,12 @@
-import { input } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { MatCardModule } from '@angular/material/card';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserCardComponent } from './user-card.component';
-
+import { User } from './user';
 describe('UserCardComponent', () => {
   let component: UserCardComponent;
   let fixture: ComponentFixture<UserCardComponent>;
-
+  let expectedUser: User;
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -23,7 +22,7 @@ describe('UserCardComponent', () => {
     fixture = TestBed.createComponent(UserCardComponent);
     component = fixture.componentInstance;
     TestBed.runInInjectionContext(() => {
-      component.user = input({
+      expectedUser = {
         _id: 'chris_id',
         name: 'Chris',
         age: 25,
@@ -31,8 +30,9 @@ describe('UserCardComponent', () => {
         email: 'chris@this.that',
         role: 'admin',
         avatar: 'https://gravatar.com/avatar/8c9616d6cc5de638ea6920fb5d65fc6c?d=identicon'
-      })
+      };
     });
+    fixture.componentRef.setInput('user', expectedUser);
     fixture.detectChanges();
   });
 
